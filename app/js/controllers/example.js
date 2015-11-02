7f6b7700692bef251b1d9e3d2ee9348bc1f1f68a
Miramac <miramac@gmx.net>
2015-11-02 18:05:56 +0100

    testi

--- app/js/controllers/example.js
+++ app/js/controllers/example.js
@@ -8,6 +8,12 @@ function ExampleCtrl() {
   vm.title = 'AngularJS, Gulp, and Browserify! Written with keyboards and love!';
   vm.number = 1234;
 
+  vm.grid = [
+      "A1", "A2", "A3",
+      "B1", "B2", "B3",
+      "C1", "C2", "C3"
+  ]
+
 }
 
 export default {
